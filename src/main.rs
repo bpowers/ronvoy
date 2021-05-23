b9c2df779856cf89b58b0908346da35e5fd0889c
Bobby Powers <bobbypowers@gmail.com>
2021-05-23 08:58:23 -0700

    deny check

--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,25 @@ use std::path::PathBuf;
 use std::sync::Arc;
 use std::net::ToSocketAddrs;
 use std::io::BufReader;
-use futures_util::future;
-use structopt::StructOpt;
-use tokio::runtime;
-use tokio::net::TcpStream;
-use tokio::io::{
-    AsyncWriteExt,
-    copy, split,
-    stdin as tokio_stdin, stdout as tokio_stdout
-};
-use tokio_rustls::{ TlsConnector, rustls::ClientConfig, webpki::DNSNameRef };
+use async_executor::Executor;
+use async_rustls::{ TlsConnector, rustls::ClientConfig, webpki::DNSNameRef };
 
-
-#[derive(StructOpt)]
 struct Options {
     host: String,
-
-    /// port
-    #[structopt(short="p", long="port", default_value="443")]
     port: u16,
-
-    /// domain
-    #[structopt(short="d", long="domain")]
     domain: Option<String>,
-
-    /// cafile
-    #[structopt(short="c", long="cafile", parse(from_os_str))]
-    cafile: Option<PathBuf>
+    ca_file: Option<PathBuf>,
 }
 
+// the root datastructure for a Ronvoy instance
+struct GlobalRonvoy<'a> {
+    // TLS handshakes are expensive - segregate them from proxying
+    // data back and forth on established connections
+    handshake_executor: Executor<'a>,
+    // once a connection is established it goes here
+    established_executor: Executor<'a>,
+
+}
 
 fn main() -> io::Result<()> {
     let options = Options::from_args();
@@ -47,42 +37,42 @@ fn main() -> io::Result<()> {
         domain
     );
 
-    let mut runtime = runtime::Builder::new()
-        .basic_scheduler()
-        .enable_io()
-        .build()?;
-    let mut config = ClientConfig::new();
-    if let Some(cafile) = &options.cafile {
-        let mut pem = BufReader::new(File::open(cafile)?);
-        config.root_store.add_pem_file(&mut pem)
-            .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid cert"))?;
-    } else {
-        config.root_store.add_server_trust_anchors(&webpki_roots::TLS_SERVER_ROOTS);
-    }
-    let connector = TlsConnector::from(Arc::new(config));
-
-    let fut = async {
-        let stream = TcpStream::connect(&addr).await?;
-
-        let (mut stdin, mut stdout) = (tokio_stdin(), tokio_stdout());
-
-        let domain = DNSNameRef::try_from_ascii_str(&domain)
-            .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid dnsname"))?;
-
-        let mut stream = connector.connect(domain, stream).await?;
-        stream.write_all(content.as_bytes()).await?;
-
-        let (mut reader, mut writer) = split(stream);
-        future::select(
-            copy(&mut reader, &mut stdout),
-            copy(&mut stdin, &mut writer)
-        )
-            .await
-            .factor_first()
-            .0?;
-
-        Ok(())
-    };
-
-    runtime.block_on(fut)
+    // let mut runtime = runtime::Builder::new()
+    //     .basic_scheduler()
+    //     .enable_io()
+    //     .build()?;
+    // let mut config = ClientConfig::new();
+    // if let Some(cafile) = &options.cafile {
+    //     let mut pem = BufReader::new(File::open(cafile)?);
+    //     config.root_store.add_pem_file(&mut pem)
+    //         .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid cert"))?;
+    // } else {
+    //     config.root_store.add_server_trust_anchors(&webpki_roots::TLS_SERVER_ROOTS);
+    // }
+    // let connector = TlsConnector::from(Arc::new(config));
+    //
+    // let fut = async {
+    //     let stream = TcpStream::connect(&addr).await?;
+    //
+    //     let (mut stdin, mut stdout) = (tokio_stdin(), tokio_stdout());
+    //
+    //     let domain = DNSNameRef::try_from_ascii_str(&domain)
+    //         .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid dnsname"))?;
+    //
+    //     let mut stream = connector.connect(domain, stream).await?;
+    //     stream.write_all(content.as_bytes()).await?;
+    //
+    //     let (mut reader, mut writer) = split(stream);
+    //     future::select(
+    //         copy(&mut reader, &mut stdout),
+    //         copy(&mut stdin, &mut writer)
+    //     )
+    //         .await
+    //         .factor_first()
+    //         .0?;
+    //
+    //     Ok(())
+    // };
+    //
+    // runtime.block_on(fut)
 }
