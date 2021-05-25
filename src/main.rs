a229c1ed4d548dc58df0fbd83232982fba136dc4
Bobby Powers <bobbypowers@gmail.com>
2021-05-25 08:15:42 -0700

    do the TLS handshake, add ./run

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,19 @@
 // Use of this source code is governed by the Apache License,
 // Version 2.0, that can be found in the LICENSE file.
 
-use std::io;
+use std::fs::File;
+use std::io::{self, BufReader};
 use std::net::ToSocketAddrs;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::sync::Arc;
 use std::thread;
 
 use async_executor::Executor;
 use async_io::block_on;
 use async_net::TcpListener;
-use async_rustls::{rustls::NoClientAuth, rustls::ServerConfig, TlsAcceptor};
+use async_rustls::rustls::internal::pemfile::{certs, pkcs8_private_keys};
+use async_rustls::rustls::{Certificate, NoClientAuth, PrivateKey, ServerConfig};
+use async_rustls::TlsAcceptor;
 use futures_lite::{future, AsyncWriteExt};
 use pico_args::Arguments;
 
@@ -43,6 +46,8 @@ fn usage() -> ! {
             "    --addr           address to listen on (defaults to 127.0.0.1)\n",
             "    --port           port to listen on (defaults to 9301)\n",
             "    --ca_file FILE   Certificate Authority file (defaults to webpki_roots)\n",
+            "    --cert FILE      server certificate to use\n",
+            "    --key FILE       server private key to use\n",
         ),
         VERSION,
         argv0
@@ -54,6 +59,8 @@ struct Args {
     addr: String,
     port: u16,
     ca_file: Option<PathBuf>,
+    cert: PathBuf,
+    key: PathBuf,
 }
 
 fn parse_args() -> Result<Args, Box<dyn std::error::Error>> {
@@ -63,17 +70,21 @@ fn parse_args() -> Result<Args, Box<dyn std::error::Error>> {
     }
 
     let mut args = Args {
-        addr: "127.0.0.1".to_string(),
+        addr: "127.0.0.1".to_owned(),
         port: 9301,
         ca_file: None,
+        cert: "".to_owned().into(),
+        key: "".to_owned().into(),
     };
     if let Ok(addr) = parsed.value_from_str("--addr") {
         args.addr = addr;
     }
-    if let Ok(port) = parsed.value_from_str::<&str, String>("port") {
+    if let Ok(port) = parsed.value_from_str::<&str, String>("--port") {
         args.port = port.parse::<u16>().unwrap();
     }
     args.ca_file = parsed.value_from_str("--ca_file").ok();
+    args.cert = parsed.value_from_str("--cert").unwrap();
+    args.key = parsed.value_from_str("--key").unwrap();
 
     Ok(args)
 }
@@ -85,6 +96,16 @@ struct GlobalRonvoy<'a> {
     handshake_executor: Arc<Executor<'a>>,
     // once a connection is established it goes here
     proxy_executor: Arc<Executor<'a>>,
+}
+
+fn load_certs(path: &Path) -> io::Result<Vec<Certificate>> {
+    certs(&mut BufReader::new(File::open(path)?))
+        .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid cert"))
+}
+
+fn load_keys(path: &Path) -> io::Result<Vec<PrivateKey>> {
+    pkcs8_private_keys(&mut BufReader::new(File::open(path)?))
+        .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid key"))
 }
 
 fn main() {
@@ -137,7 +158,12 @@ fn main() {
             .expect("cannot spawn executor thread");
     }
 
-    let config = ServerConfig::new(NoClientAuth::new());
+    let certs = load_certs(&args.cert).unwrap();
+    let mut keys = load_keys(&args.key).unwrap();
+    let mut config = ServerConfig::new(NoClientAuth::new());
+    config
+        .set_single_cert(certs, keys.remove(0))
+        .map_err(|err| io::Error::new(io::ErrorKind::InvalidInput, err)).unwrap();
 
     let addr = (args.addr.as_str(), args.port)
         .to_socket_addrs()
@@ -148,7 +174,7 @@ fn main() {
 
     let acceptor = TlsAcceptor::from(Arc::new(config));
     let handshake_executor = global_ronvoy.handshake_executor.clone();
-    let _proxy_executor = global_ronvoy.proxy_executor.clone();
+    let proxy_executor = global_ronvoy.proxy_executor.clone();
 
     let fut = async move {
         let listener = TcpListener::bind(addr).await.unwrap();
@@ -156,14 +182,29 @@ fn main() {
         loop {
             let (stream, peer_addr) = listener.accept().await.unwrap();
             let acceptor = acceptor.clone();
+            let proxy_executor = (&proxy_executor).clone();
 
-            eprintln!("accepted new connection from {}", peer_addr);
+            eprintln!(
+                "accepted new connection from {} ({:?})",
+                peer_addr,
+                thread::current().id()
+            );
             let fut = async move {
                 let mut stream = acceptor.accept(stream).await?;
+                let fut = async move {
+                    eprintln!(
+                        "we accepted, what now? closing. ({:?})",
+                        thread::current().id()
+                    );
+                    stream.close().await.unwrap();
+                };
 
-                eprintln!("we accepted, what now?  need to move it to the next executor");
-
-                stream.close().await.unwrap();
+                // move the rest of computation onto the proxy thread pool
+                // (alternatively, pick one of $n independent executors to
+                // move it to rather than a global pool, like envoy, in the
+                // future)
+                let proxy_executor = (&proxy_executor).clone();
+                proxy_executor.spawn(fut).detach();
 
                 Ok::<(), Box<dyn std::error::Error>>(())
             };
@@ -171,7 +212,7 @@ fn main() {
             handshake_executor
                 .spawn(async move {
                     if let Err(err) = fut.await {
-                        eprintln!("listener err: {}", err)
+                        eprintln!("listener err: {} ({:?})", err, thread::current().id())
                     }
                 })
                 .detach();
