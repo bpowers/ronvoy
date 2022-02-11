b8c09cae10cda0fb5f8561b192ab8bd5126a5bdc
Bobby Powers <bobbypowers@gmail.com>
2022-02-11 09:34:12 -0800

    upstream-service: use TcpListenerCloner and jemalloc

--- test/upstream-service/src/main.rs
+++ test/upstream-service/src/main.rs
@@ -2,35 +2,30 @@
 // Use of this source code is governed by the Apache License,
 // Version 2.0, that can be found in the LICENSE file.
 
-use std::net::{SocketAddr, TcpListener};
+#[global_allocator]
+static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
+use std::net::SocketAddr;
 
 use axum::{routing::get, Router};
-use socket2::SockAddr;
+use ronvoy_core::net::TcpListenerCloner;
 
 fn main() {
     let num_threads = {
         std::env::var("CONCURRENCY")
             .ok()
             .and_then(|s| s.parse().ok())
-            .unwrap_or(4)
+            .unwrap_or(8)
     };
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 9110));
     println!("listening on {}", addr);
 
+    let mut listener = TcpListenerCloner::new(addr);
+
     let mut children = vec![];
     for _i in 0..num_threads {
-        let socket = socket2::Socket::new(
-            socket2::Domain::IPV4,
-            socket2::Type::STREAM,
-            Some(socket2::Protocol::TCP),
-        )
-        .unwrap();
-        socket.set_reuse_address(true).unwrap();
-        socket.set_reuse_port(true).unwrap();
-        socket.bind(&SockAddr::from(addr)).unwrap();
-        socket.listen(128).unwrap();
-        let listener = TcpListener::from(socket);
+        let listener = listener.clone_listener().unwrap();
 
         children.push(std::thread::spawn(move || {
             tokio::runtime::Builder::new_current_thread()
